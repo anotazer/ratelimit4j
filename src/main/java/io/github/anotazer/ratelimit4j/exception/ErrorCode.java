c5d1e7a15c2e939c9a8ebb6a5e90e62081dd98b0
kwnoyng <kwnoyng@gmail.com>
2024-10-15 01:01:01 +0900

    feat: Cookie & QueryString strategy

--- src/main/java/io/github/anotazer/ratelimit4j/exception/ErrorCode.java
+++ src/main/java/io/github/anotazer/ratelimit4j/exception/ErrorCode.java
@@ -6,6 +6,8 @@ public enum ErrorCode {
     BAD_REQUEST("400", "Bad Request"),
     TOO_MANY_REQUESTS("429", "Too many requests"),
 
+    KEY_NAME_NOT_FOUND("KEY_001", "The specified key name does not exist"),
+    COOKIE_NOT_FOUND("COOKIE_001", "The specified cookie does not exist"),
     INTERNAL_SERVER_ERROR("500", "Internal server error");
 
     private final String code;
