e4a9c58c846bb6b5c8ad439db882eced0eb1649b
kwnoyng <kwnoyng@gmail.com>
2024-10-27 16:16:14 +0900

    fix: Make Spring Boot Application main file

---
+++ src/main/java/io/github/anotazer/ratelimit4j/Ratelimit4jApplication.java
@@ -0,0 +1,11 @@
+package io.github.anotazer.ratelimit4j;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ratelimit4jApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(Ratelimit4jApplication.class, args);
+    }
+}
