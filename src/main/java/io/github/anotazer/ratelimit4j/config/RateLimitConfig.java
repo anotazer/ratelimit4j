4b723765ffedc24027b3b7414d32d5b8ef523a79
dojsfffff <dojsfffff@naver.com>
2024-11-17 12:25:20 +0900

    feat: add autoConfiguration setting

---
+++ src/main/java/io/github/anotazer/ratelimit4j/config/RateLimitConfig.java
@@ -0,0 +1,18 @@
+package io.github.anotazer.ratelimit4j.config;
+
+import io.github.anotazer.ratelimit4j.aspect.RateLimitAspect;
+import io.github.anotazer.ratelimit4j.service.RateLimitService;
+import org.springframework.boot.autoconfigure.AutoConfiguration;
+import org.springframework.context.annotation.Bean;
+
+@AutoConfiguration
+public class RateLimitConfig {
+  @Bean
+  public RateLimitService rateLimitService() {
+    return new RateLimitService();
+  }
+  @Bean
+  public RateLimitAspect rateLimitAspect(RateLimitService rateLimitService) {
+    return new RateLimitAspect(rateLimitService);
+  }
+}
